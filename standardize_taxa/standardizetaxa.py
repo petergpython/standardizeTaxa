add87d3aefe9d4fe14b61733b48c840260661542
Peter Giovannini <peter.giovannini@gmail.com>
2025-03-24 16:03:57 +0100

    Update standardizetaxa.py

--- standardize_taxa/standardizetaxa.py
+++ standardize_taxa/standardizetaxa.py
@@ -35,14 +35,15 @@ def extract_best_result(list_res):
         if match_type != "NoMatch": 
            input_name  = i['names'][0]['name']
            matched_name = i['names'][0]['bestResult']['matchedName']
+           source_used = i['names'][0]['bestResult']['dataSourceTitleShort']
            output_name = i['names'][0]['bestResult']["currentName"]
            status = i['names'][0]['bestResult']['taxonomicStatus']
-           final.append((input_name, matched_name, match_type, status, output_name))
+           final.append((input_name, matched_name, match_type, status, output_name, source_used))
         else: 
-           final.append((i['names'][0]['name'], 'no_match', 'no_match', 'no_match', 'no_match'))
+           final.append((i['names'][0]['name'], 'no_match', 'no_match', 'no_match', 'no_match', 'no_match'))
     return final
 
-def run_batch_queries(df, threshold = 0.8):    
+def run_batch_queries(df, threshold = 0.8, sources = ['196']):    
     """
     take a list of taxa and return a list of queries results
     """
@@ -56,20 +57,23 @@ def run_batch_queries(df, threshold = 0.8):
         counter += 1
     return result_queries
 
-def standardize(input_df, threshold = 0.8):
+def standardize(input_df, threshold = 0.8, sources = ['196']):
     """
-    takes a dataframe return a dataframe
-    column with taxa name to be standardized must be called "full_taxa_input"
-    threshold argument from 0.1 to 0.99 is optional 
+    arguments: 1) a dataframe with a column with taxa name to be standardized called "full_taxa_input"
+    2)threshold argument from 0.1 to 0.99 is optional 
+    3)sources , a list with the code of the database(s) to be used for ex '196' for World Flora Online 
     """
+    results = dict()
     result_queries = run_batch_queries(input_df, threshold)
     res = extract_best_result(result_queries)
     # create a taxa dictionary to be used to map the standardised taxa to the taxa in the dataset
     taxa_standardized_df = pd.DataFrame(res)
-    taxa_standardized_df.columns = ['input_name' , 'matched_name', 'match_type', 'status', 'output_name']  
+    taxa_standardized_df.columns = ['input_name' , 'matched_name', 'match_type', 'status', 'output_name', 'source_used']  
     taxa_dictionary = taxa_standardized_df.set_index('input_name')[['output_name']].to_dict()['output_name']
     # add standardized taxa to the combined dataset
     input_df['taxa_standardized'] = input_df.full_taxa_input.map(taxa_dictionary)
-    return input_df
+    results['dataframe'] = input_df
+    results['taxa_reconciliation_table'] = taxa_standardized_df
+    return results
 
 
